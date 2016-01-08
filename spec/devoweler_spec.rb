4ef12bf8cbe21972879b477ce7fcb392a6e11573
patsul12 <patsul12@gmail.com>
2016-01-08 08:35:44 -0800

    add test to ensure characters that are not vowels are not being changed

--- spec/devoweler_spec.rb
+++ spec/devoweler_spec.rb
@@ -23,5 +23,8 @@ describe("String#devoweler") do
   it("replaces 'u' with '-'") do
     expect("u".devoweler).to(eq("-"))
   end
-  
+
+  it("does not alter chars that are not vowels") do
+    expect("qwrtypsdfghjklzxcvbnm1234567890-=,./;'[]'!@#$%^&*()_{}").to(eq("qwrtypsdfghjklzxcvbnm1234567890-=,./;'[]'!@#$%^&*()_{}"))
+  end
 end
