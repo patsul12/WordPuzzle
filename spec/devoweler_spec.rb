43ecfb93c7fd797d5e6d843ceb358d05df75e91e
patsul12 <patsul12@gmail.com>
2016-01-08 08:29:29 -0800

    add tests checking for replacement of single vowels

---
+++ spec/devoweler_spec.rb
@@ -0,0 +1,27 @@
+require('rspec')
+require('devoweler')
+
+describe("String#devoweler") do
+
+  # Tests for replacement of single char vowel strings
+  it("replaces 'a' with '-'") do
+    expect("a".devoweler).to(eq("-"))
+  end
+
+  it("replaces 'e' with '-'") do
+    expect("e".devoweler).to(eq("-"))
+  end
+
+  it("replaces 'i' with '-'") do
+    expect("i".devoweler).to(eq("-"))
+  end
+
+  it("replaces 'o' with '-'") do
+    expect("o".devoweler).to(eq("-"))
+  end
+
+  it("replaces 'u' with '-'") do
+    expect("u".devoweler).to(eq("-"))
+  end
+  
+end
