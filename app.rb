eb80ebf8bfc314f2c56d2460fcbd95812b941ad2
patsul12 <patsul12@gmail.com>
2016-01-08 11:39:45 -0800

    fix title var not appearing on page

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/devoweler.rb')
 
-@title = "Word Puzzle"
-
 get('/') do
+  @title = "Word Puzzle"
   @page_header = "Welcome to the Word Puzzle App!"
   erb(:index)
 end
 
 get('/puzzle') do
+  @title = "Word Puzzle"
   @page_header = "Can you solve the puzzle?"
   @answer = params.fetch('input')
   @puzzle = @answer.devoweler
